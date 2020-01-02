22382b5f6871751dcb5685b0e898e0c1b19486e5
Sourav Rane <44705241+Sourav-Rane@users.noreply.github.com>
2020-01-02 12:28:00 +0530

    update

---
+++ CoreJava/Project 3/src/Exercise/FileExe.java
@@ -0,0 +1,25 @@
+package Exercise;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExe {
+	public static void main(String[] args) throws IOException {
+		File f = new File("C:\\Users\\Admin\\Documents\\GitHub\\Deloitte\\File\\Batch\\BatchMates.txt");
+		File folder = new File("C:\\Users\\Admin\\Documents\\GitHub\\Deloitte\\File\\Batch");
+
+		if (f.exists())
+			System.out.println("File is there ... go and see ");
+		else {
+			folder.mkdir(); // to create a folder
+			f.createNewFile(); // to create a file
+			System.out.println("New File created !");
+		}
+		File path = new File("C:\\\\Users\\\\Admin\\\\Documents\\\\GitHub\\\\Deloitte\\\\File");
+		File arr[] = path.listFiles();
+
+		for (File a : arr)
+			System.out.println(a);
+
+	}
+}
